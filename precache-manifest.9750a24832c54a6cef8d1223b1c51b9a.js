123ce1928a59536ee891dead95d17d945ca7cb13
Adrian Velazquez <avelazquez585@gmail.com>
2019-07-17 18:36:52 -0400

    Updates

---
+++ precache-manifest.9750a24832c54a6cef8d1223b1c51b9a.js
@@ -0,0 +1,170 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "af65ff30c2300944f22e3883eb38fb8b",
+    "url": "/React-app-portfolio/index.html"
+  },
+  {
+    "revision": "e01a907cb68f693892f4",
+    "url": "/React-app-portfolio/static/css/main.0add3b37.chunk.css"
+  },
+  {
+    "revision": "6253f51732b89352c323",
+    "url": "/React-app-portfolio/static/js/2.559c6a89.chunk.js"
+  },
+  {
+    "revision": "e01a907cb68f693892f4",
+    "url": "/React-app-portfolio/static/js/main.8fbeeab9.chunk.js"
+  },
+  {
+    "revision": "9a6f66462eaf89348c74",
+    "url": "/React-app-portfolio/static/js/runtime~main.61e1ee24.js"
+  },
+  {
+    "revision": "f1037b8138333d76a1289d15fc4a9b05",
+    "url": "/React-app-portfolio/static/media/Android.f1037b81.zip"
+  },
+  {
+    "revision": "af34f7f0961f7f380cb9e8ebce525c42",
+    "url": "/React-app-portfolio/static/media/Elizabeth_Arden.af34f7f0.zip"
+  },
+  {
+    "revision": "a93f46282f2d5f4215bd472d4a96a7c6",
+    "url": "/React-app-portfolio/static/media/Google.a93f4628.zip"
+  },
+  {
+    "revision": "9254f75168d7e895851ba89df8787034",
+    "url": "/React-app-portfolio/static/media/Mini_Cooper.9254f751.zip"
+  },
+  {
+    "revision": "786c18b92e12e1e217b62f3ed2befbd8",
+    "url": "/React-app-portfolio/static/media/TMobile.786c18b9.zip"
+  },
+  {
+    "revision": "0df6e87e49e25d6940f883e78b59f71f",
+    "url": "/React-app-portfolio/static/media/business_model.0df6e87e.pdf"
+  },
+  {
+    "revision": "02678207ab770c6ca78117b3f4aca924",
+    "url": "/React-app-portfolio/static/media/education_history_fullsail_bachelors.02678207.png"
+  },
+  {
+    "revision": "8cbc90eb5b83277f7529a5b50888b9d7",
+    "url": "/React-app-portfolio/static/media/education_history_fullsail_masters.8cbc90eb.jpg"
+  },
+  {
+    "revision": "458dc331e802342a54c3ddbd240e60e2",
+    "url": "/React-app-portfolio/static/media/education_history_uri.458dc331.jpg"
+  },
+  {
+    "revision": "1238486b073d03db9cb2589222584760",
+    "url": "/React-app-portfolio/static/media/education_history_uri_icon.1238486b.png"
+  },
+  {
+    "revision": "a0f93836f0701e0750291a4b15b8bfed",
+    "url": "/React-app-portfolio/static/media/fb.a0f93836.png"
+  },
+  {
+    "revision": "76d344f08412543c74ccd1fb2493d607",
+    "url": "/React-app-portfolio/static/media/github.76d344f0.svg"
+  },
+  {
+    "revision": "05a3e30fdf60ebe9d00cf4725d44bd42",
+    "url": "/React-app-portfolio/static/media/icon.05a3e30f.svg"
+  },
+  {
+    "revision": "d6d03e41e2d20c02d0e687ec5e2349b9",
+    "url": "/React-app-portfolio/static/media/intro.d6d03e41.jpg"
+  },
+  {
+    "revision": "4b475fb3f54b3e35daa04295ec48df4c",
+    "url": "/React-app-portfolio/static/media/linkedin.4b475fb3.svg"
+  },
+  {
+    "revision": "a226a9ba5f94a6ec8a89fcc4bacf5e47",
+    "url": "/React-app-portfolio/static/media/me.a226a9ba.jpg"
+  },
+  {
+    "revision": "4e89bb819832648308c38ff8fee5f1f2",
+    "url": "/React-app-portfolio/static/media/resume.4e89bb81.pdf"
+  },
+  {
+    "revision": "93e6cd8a754abf5e9e212a03edb94504",
+    "url": "/React-app-portfolio/static/media/sample_google_icon.93e6cd8a.png"
+  },
+  {
+    "revision": "355c8854e786c7bf6148230eacfdd55e",
+    "url": "/React-app-portfolio/static/media/sample_mini_icon.355c8854.png"
+  },
+  {
+    "revision": "5ac110bc299805eccab1e00a17891362",
+    "url": "/React-app-portfolio/static/media/sample_riot_icon.5ac110bc.png"
+  },
+  {
+    "revision": "4696b0a2dc948f04ba844822afa51211",
+    "url": "/React-app-portfolio/static/media/sample_tmobile_icon.4696b0a2.png"
+  },
+  {
+    "revision": "d938576565edfb7f36b99bd1075122c7",
+    "url": "/React-app-portfolio/static/media/stack_ai_icon.d9385765.png"
+  },
+  {
+    "revision": "03a386d452e0416f43440a762c0096d9",
+    "url": "/React-app-portfolio/static/media/stack_css_icon.03a386d4.svg"
+  },
+  {
+    "revision": "311d59a66f40f52d1584ad3d6033e6bb",
+    "url": "/React-app-portfolio/static/media/stack_greensock_icon.311d59a6.svg"
+  },
+  {
+    "revision": "454a400ca987d4271e54045982ea3876",
+    "url": "/React-app-portfolio/static/media/stack_html_icon.454a400c.png"
+  },
+  {
+    "revision": "515a82daeb80c805b8af3c0475708e0e",
+    "url": "/React-app-portfolio/static/media/stack_jquery_icon.515a82da.png"
+  },
+  {
+    "revision": "e8c27f6b0fcf596a06a31135d937c73e",
+    "url": "/React-app-portfolio/static/media/stack_js_icon.e8c27f6b.png"
+  },
+  {
+    "revision": "220d6f6b61c18f2244a008fd5cbf5d1f",
+    "url": "/React-app-portfolio/static/media/stack_json_icon.220d6f6b.png"
+  },
+  {
+    "revision": "84dae0c85887a8c7798e1a0df84fd34e",
+    "url": "/React-app-portfolio/static/media/stack_node_icon.84dae0c8.png"
+  },
+  {
+    "revision": "cb04570a6cff29d07fac65cb17d0106e",
+    "url": "/React-app-portfolio/static/media/stack_npm_icon.cb04570a.png"
+  },
+  {
+    "revision": "446763959684b108ca164f3cdc9c96d0",
+    "url": "/React-app-portfolio/static/media/stack_ps_icon.44676395.svg"
+  },
+  {
+    "revision": "1f1465694012cf3b08e8c818a5378db9",
+    "url": "/React-app-portfolio/static/media/stack_wp_icon.1f146569.png"
+  },
+  {
+    "revision": "6203ec9eedd6190a0754b426462b1882",
+    "url": "/React-app-portfolio/static/media/tools.6203ec9e.svg"
+  },
+  {
+    "revision": "182d5ef4d173a3cd93897242a6fd56a5",
+    "url": "/React-app-portfolio/static/media/twitter.182d5ef4.svg"
+  },
+  {
+    "revision": "c3e837ecf5a1a4826c87283fd35070fd",
+    "url": "/React-app-portfolio/static/media/work_history_bannerboy.c3e837ec.jpg"
+  },
+  {
+    "revision": "765086a300cde10987da9dc925e976e0",
+    "url": "/React-app-portfolio/static/media/work_history_droga5.765086a3.jpg"
+  },
+  {
+    "revision": "72dbbf477d473121f407612889094212",
+    "url": "/React-app-portfolio/static/media/work_history_revlon.72dbbf47.jpeg"
+  }
+]);
